9a25661297248c3edd5b8686aeb1f70d0c10e827
Ttgamer2207 <gamerman.yala@gmail.com>
2024-06-13 15:26:47 +0300

    Second commit

--- app.js
+++ app.js
@@ -1,13 +1,1 @@
-const http = require('http')
-
-const server = http.createServer((req,res)=>{
-    if (req.url ==='/') {
-        res.end('Welcome to the home page')
-        }
-    else if (req.url==='/about') {
-        res.end('Welcome to the about page')
-    }
-    res.end('<h1>Woops</h1>')
-})
-
-server.listen(3010, 'localhost')
+console.log('shit people');
